f311c151f84314b9a218ee7d32b5a8b1c6158e60
Arka <arkam27112004@gmail.com>
2024-04-17 16:00:57 +0530

    improve ques 5

--- LAB5/B23120-Assignment05-IC252-Q5.py
+++ LAB5/B23120-Assignment05-IC252-Q5.py
@@ -6,13 +6,12 @@ POINTS = 1000
 _y = []
 current_pi = 0
 fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
-ax[0].add_patch(plt.Circle((0.5, 0.5), 0.5, color='black', fill=False))
+ax[0].add_patch(plt.Circle((0, 0), 1, color='black', fill=False))
 ax[1].set_title(f"Evolution of pi estimation")
 ax[1].axhline(y=np.pi, color='red', linestyle='--')
 
 for _ in range(POINTS):
-    x = np.random.rand()
-    y = np.random.rand()
+    x, y = np.random.rand(2)*2-1
     if x**2 + y**2 <= 1:
         current_pi += 1
     _y += [current_pi*4/(_+1)]
